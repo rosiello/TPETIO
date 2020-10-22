82e37e3c3ee46984bab6c9aadae901c789db3369
Cristian Amici <cristianamici@gmail.com>
2020-10-22 00:09:09 -0300

    El router avanzado esta malardo lo mejor es borrar todo

--- RouterAvanzado.php
+++ RouterAvanzado.php
@@ -9,15 +9,7 @@ define ("BASE_URL", '//'.$_SERVER["SERVER_NAME"] . ':' . $_SERVER["SERVER_PORT"]
 $r = new Router();
 
 //Navegacion//
-$r->addRoute("Home", "GET","Controller","Home"); 
-$r->addRoute("galeria", "GET","Controller","galeria");
-$r->addRoute("tecnicas", "GET","Controller","tecnicas");
-$r->addRoute("nosotros", "GET","Controller","nosotros");
-$r->addRoute("contacto", "GET","Controller","home");
-$r->addRoute("agradecimientos", "GET","Controller","home");
-$r->addRoute("login", "GET","Controller","login");
-$r->addRoute("artistas", "GET", "Controller","artistas");
-  
+
   //////////////////DEFAULT/////////////////////
 $r->route($_GET['action'], $_SERVER['REQUEST_METHOD']);
 $r->setDefaultRoute("Controller", "Home");
